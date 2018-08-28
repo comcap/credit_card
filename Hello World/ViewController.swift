f4c438b9b4292fac99be49b6276df8aa6433b837
Chalermwut <a.30861@hotmail.com>
2018-08-28 17:13:11 +0700

    create app

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by IOS Developer on 28/8/2561 BE.
+//  Copyright © 2561 IOS Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
